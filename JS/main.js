161042b126f3f5d013090e61c896f391f50e7a9a
vusalnasirli <7bgzt9j@code.edu.az>
2022-05-16 13:16:28 +0400

    sd

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+// let name = document.querySelector("header");
+// console.log(name);
+
+
+// let title = document.querySelector("#header h1");
+// console.log(title);
+
+
+// let ctr = document.querySelector("#header .main");
+// console.log(ctr);
+
+let changebtn = document.querySelector( " .btn ")
+let red = document.querySelector(".main")
+
+changebtn.addEventListener('click' , () =>
+ {
+     red.style.backgroundColor='orange'
+ }
+) 
+
+let newone =document.querySelector("newone")
+let nameinput = document.querySelector(".name-mb-3")
+nameinput.addEventListener('change', (e) =>
+{
+    fu
+}
+)
+
+
+let surnameinput = document.querySelector(".surname-mb-3")
+let email = document.querySelector("mail-mb-3")
+let password = document.querySelector("password-mb-3")
